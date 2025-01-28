7eedce8ec9f0a947a946cd7fb0ed3baabb0acd55
Daniel Eradilov <dan4oeradilov@gmail.com>
2025-01-28 11:40:22 +0500

    Task 2

--- lab03/functions1.py
+++ lab03/functions1.py
@@ -1,8 +1,17 @@
-# 1.
+'''# 1.
 
 def ingredient(grams):
     ounces = 28.3495231 * grams
     return(ounces)
 
 grams = float(input("How many grams: "))
-print(ingredient(grams))
+print(ingredient(grams))'''
+
+# 2.
+
+def FahrenheittoCelcius(F):
+    C = (5/9) * (F-32)
+    return C
+
+F = float(input("Temperature in Fahrenheit: "))
+print(f"Temperature in Celsius: {FahrenheittoCelcius(F)}")
