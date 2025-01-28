c0cc773714d7a6a295ec49e9bbb32900c5111950
Daniel Eradilov <dan4oeradilov@gmail.com>
2025-01-28 12:08:42 +0500

    Task 3

--- lab03/functions1.py
+++ lab03/functions1.py
@@ -7,11 +7,23 @@ def ingredient(grams):
 grams = float(input("How many grams: "))
 print(ingredient(grams))'''
 
-# 2.
+'''# 2.
 
 def FahrenheittoCelcius(F):
     C = (5/9) * (F-32)
     return C
 
 F = float(input("Temperature in Fahrenheit: "))
-print(f"Temperature in Celsius: {FahrenheittoCelcius(F)}")
+print(f"Temperature in Celsius: {FahrenheittoCelcius(F)}")'''
+
+# 3
+
+def solve(numheads, numlegs):
+    rabbits = int((numlegs - 2*numheads)/2)
+    chickens = int(numheads - rabbits)
+    print(f'Number of rabbits = {rabbits} \nNumber of chickens = {chickens}')
+
+numheads = int(input('Number of heads: '))
+numlegs = int(input('Number of legs: '))
+
+solve(numheads,numlegs)
