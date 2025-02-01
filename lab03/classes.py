153639f94ff2d02a38d056b53ac2cc5ef0e98c26
Daniel Eradilov <dan4oeradilov@gmail.com>
2025-02-01 11:29:47 +0500

    Task 4

--- lab03/classes.py
+++ lab03/classes.py
@@ -42,5 +42,32 @@ class Rectangle(Shape):
     def area(self):
         return self.length * self.widht
 
-abcd = Rectangle(5,4)
-print(abcd.area())
+# abcd = Rectangle(5,4)
+# print(abcd.area())
+
+# 4
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+    
+    def show(self):
+        text = 'Coordinates of the specified point: ('+str(self.x)+';'+str(self.y)+')'
+        return text
+    
+    def move(self, plus_x, plus_y):
+        self.x += plus_x
+        self.y += plus_y
+        
+        text = 'Now the coordinates of the specified point: ('+str(self.x)+';'+str(self.y)+')'
+        return text
+    
+    def dist(self):
+        text = 'Distance between x and y: ' + str((self.x**2 + self.y**2)**0.5)
+        return text
+
+# to4ka = Point(5,10)
+# print(to4ka.show())
+# print(to4ka.move(5,15))
+# print(to4ka.show())
+# print(to4ka.dist())
