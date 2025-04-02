9c60b14ba8efc8da880c9f102b483ee472e02b76
Daniel Eradilov <dan4oeradilov@gmail.com>
2025-04-02 18:23:13 +0500

    task 1 finish

--- lab07/clock.py
+++ lab07/clock.py
@@ -1,59 +1,52 @@
 import pygame
-import time
-import math
+import os
+import datetime
+import sys  # Импортируем модуль sys для завершения программы
+
+_image_library = {}
+def get_image(path):
+    global _image_library 
+    image = _image_library.get(path)
+    if image == None:
+        canonicalized_path = path.replace("/",os.sep).replace("\\",os.sep)
+        image = pygame.image.load(canonicalized_path)
+        _image_library[canonicalized_path] = image
+    return image
+
+def blitRotate(screen , img , pos , angle):
+    rotated_img = pygame.transform.rotate(img , angle)
+    new_rect = rotated_img.get_rect(center = img.get_rect(center = pos).center)
+    screen.blit(rotated_img, new_rect.topleft)
 
 pygame.init()
+done = False
 
-width, height = 400, 400
-screen = pygame.display.set_mode((width, height))
+screen = pygame.display.set_mode((1024,720))
+w , h = screen.get_size()
+bg = pygame.transform.scale(get_image("c:/Users/eradi/Desktop/pp2/pp2-labs/lab07/images/mainclock.png"), (w,h))
 pygame.display.set_caption("Mickey's Clock")
 
-mickey_image = pygame.image.load('mickey_clock_face.png')
+angle_min = 0
+angle_sec = 0
+global topleft
+topleft = (w//2 , h//2 )
 
-minute_hand = pygame.image.load('minute_hand.png')
-second_hand = pygame.image.load('second_hand.png')
+while not done :
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT :  # Если окно закрыто
+            done = True
 
-clock = pygame.time.Clock()
+    today = datetime.datetime.now()
+    minutes = int(datetime.datetime.strftime(today,'%M'))
+    seconds = int(datetime.datetime.strftime(today,'%S')) 
+    pos = (screen.get_width()/2 , screen.get_height()/2 )
+    screen.blit( bg , (0,0) )
 
-def rotate_image(image, angle, center):
-    """ Rotates the image by the given angle around the center point """
-    rotated_image = pygame.transform.rotate(image, angle)
-    new_rect = rotated_image.get_rect(center=center)
-    return rotated_image, new_rect
-
-def get_minute_angle(minutes):
-    return (minutes / 60) * 360
-
-def get_second_angle(seconds):
-    return (seconds / 60) * 360
-
-running = True
-while running:
-    screen.fill((255, 255, 255))
-
-    current_time = time.localtime()
-    seconds = current_time.tm_sec
-    minutes = current_time.tm_min
-
-    center = (width // 2, height // 2)
-
-    minute_angle = get_minute_angle(minutes) - 90
-    second_angle = get_second_angle(seconds) - 90
-
-    rotated_minute_hand, minute_rect = rotate_image(minute_hand, minute_angle, center)
-    rotated_second_hand, second_rect = rotate_image(second_hand, second_angle, center)
-
-    screen.blit(mickey_image, (0, 0))
-
-    screen.blit(rotated_minute_hand, minute_rect)
-    screen.blit(rotated_second_hand, second_rect)
-
+    blitRotate(screen , get_image("c:/Users/eradi/Desktop/pp2/pp2-labs/lab07/images/second_hand.png") , pos ,angle_sec )
+    blitRotate(screen , get_image("c:/Users/eradi/Desktop/pp2/pp2-labs/lab07/images/minute_hand.png"), pos , angle_min)
+    angle_min = -6*minutes-53
+    angle_sec = -6*seconds
     pygame.display.flip()
 
-    clock.tick(60)
-
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-
 pygame.quit()
+sys.exit()
