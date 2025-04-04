1457bd1d6edaea3b92256454ad0792a70b66a31c
Daniel Eradilov <dan4oeradilov@gmail.com>
2025-04-04 19:09:12 +0500

    add songs for music player

--- lab07/daniel_music.py
+++ lab07/daniel_music.py
@@ -7,10 +7,10 @@ pygame.init()
 window = pygame.display.set_mode((720, 480))
 pygame.display.set_caption("Music Player")
 
-# Песни/музыки
+# Песни
 songs = [
-    'musics/song1.mp3', 
-    'musics/song2.mp3',
+    'c:/Users/eradi/Desktop/pp2/pp2-labs/lab07/musics/song1.mp3', 
+    'c:/Users/eradi/Desktop/pp2/pp2-labs/lab07/musics/song2.mp3',
 ]
 
 # Для старта
@@ -19,7 +19,7 @@ current_song = random.choice(songs)
 pygame.mixer.music.load(current_song)
 pygame.mixer.music.play()
 
-# Функции для управления песнями
+# Управление
 def play_next_song(): # Следующяя
     global songs, current_song
     songs = songs[1:] + [songs[0]]
@@ -35,15 +35,19 @@ def play_previous_song(): # Предыдущяя
     pygame.mixer.music.play()
 
 # Интерфейс
-button_size = (173, 173)
+button_size = (128, 128)
 button_surface = pygame.Surface(button_size)
-button_play_img = pygame.image.load('images/play.jpg')
-button_stop_img = pygame.image.load('images/stop.jpg')
-button_next_img = pygame.image.load('images/next.jpg')
-button_previous_img = pygame.image.load('images/previous.jpg')
+button_play_img = pygame.image.load('c:/Users/eradi/Desktop/pp2/pp2-labs/lab07/images/play.jpg')
+button_stop_img = pygame.image.load('c:/Users/eradi/Desktop/pp2/pp2-labs/lab07/images/stop.jpg')
+button_next_img = pygame.image.load('c:/Users/eradi/Desktop/pp2/pp2-labs/lab07/images/next.jpg')
+button_previous_img = pygame.image.load('c:/Users/eradi/Desktop/pp2/pp2-labs/lab07/images/past.jpg')
 
 def draw_button(image, x, y):
     window.blit(image, (x, y))
+
+# Обработчик кликов
+def button_clicked(x, y, width, height, mx, my):
+    return x <= mx <= x + width and y <= my <= y + height
 
 while True:
     for event in pygame.event.get():
@@ -51,18 +55,23 @@ while True:
             pygame.quit()
             sys.exit()
 
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_p:  # Play/Pause
+        if event.type == pygame.MOUSEBUTTONDOWN:  # Проверка на клик
+            mx, my = pygame.mouse.get_pos()
+
+            # Play/pause
+            if 300 <= mx <= 300 + button_size[0] and 50 <= my <= 50 + button_size[1]:
                 if is_playing:
                     pygame.mixer.music.pause()
                 else:
                     pygame.mixer.music.unpause()
                 is_playing = not is_playing
 
-            elif event.key == pygame.K_n:  # -->
+            # Next
+            if 500 <= mx <= 500 + button_size[0] and 50 <= my <= 50 + button_size[1]:
                 play_next_song()
 
-            elif event.key == pygame.K_b:  # <--
+            # Past
+            if 100 <= mx <= 100 + button_size[0] and 50 <= my <= 50 + button_size[1]:
                 play_previous_song()
 
     window.fill((255, 255, 255))
